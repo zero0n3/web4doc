706eccaab2bb514f44d17c7e67e6f8a6cf2a0879
zero0n3 <ilsimonone@gmail.com>
2019-07-07 23:03:10 +0200

    altro

--- resources/views/athlete/dashboard.blade.php
+++ resources/views/athlete/dashboard.blade.php
@@ -4,8 +4,38 @@
 
 @section('content')
 
-  <h4>Dashboard {{$athlete->name}}</h4>
+<table>
+  <tr>
+    <th colspan="3"><h4>Dashboard {{$athlete->name}}</h4></th>
+    <th><a href="/athlete/{{$athlete->id}}/edit" class="waves-effect waves-light btn-small"><i class="tiny material-icons left">edit</i>UPDATE</a></th>
+  </tr>
+  <tr>
+    <td>  <div class="section">
+    <h5><h5>Data di nascita</h5>
+    <p><h6>{{date('d-m-Y', strtotime($athlete->dob))}}</h6></p>
+  </div></td>
+    <td>  <div class="section">
+    <h5><h5>Sesso</h5>
+    <p><h6>{{$athlete->sex}}</h6></p>
+  </div></td>
+    <td>  <div class="section">
+    <h5><h5>Società</h5>
+    <p><h6>{{$athlete->company->company_name}}</h6></p>
+  </div></td></td>
+  </tr>
+</table>
 
+ <div class="divider"></div>
 
+ <table>
+  <tr>
+    th
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
 
 @endsection
