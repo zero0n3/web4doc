fd0496a2ceaa32150eca4004575c52f117543a90
zero0n3 <ilsimonone@gmail.com>
2019-10-16 12:01:58 +0200

    update ottobre heroku no footer

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -42,21 +42,21 @@ $debugbar["messages"]->addMessage("hello world!");
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">Lorem Ipsus</p>
+          <h5 class="white-text"></h5>
+          <p class="grey-text text-lighten-4"></p>
 
 
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
+          <h5 class="white-text"></h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!"></a></li>
           </ul>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
+          <h5 class="white-text"></h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!"></a></li>
           </ul>
         </div>
       </div>
