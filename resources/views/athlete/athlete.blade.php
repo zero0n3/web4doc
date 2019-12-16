3ffba11fc50b937f9720aabdcfb4674e78fc6d22
zero0n3 <ilsimonone@gmail.com>
2019-12-16 14:38:36 +0100

    align

--- resources/views/athlete/athlete.blade.php
+++ resources/views/athlete/athlete.blade.php
@@ -49,7 +49,7 @@
             <td><a href="{{ route('athlete.show', ['athlete' => $athlete->id]) }}">{{$athlete->name}}</a></td>
             <td>{{date('d-m-Y', strtotime($athlete->dob))}}</td>
             <td>{{$athlete->sex}}</td>
-            <th>{{$athlete->checkups->count()}}</th>
+            <td align= "right">{{$athlete->checkups->count()}}</td>
              <td><a href="/athlete/{{$athlete->id}}/edit" class="waves-effect waves-light btn-small"><i class="tiny material-icons left">edit</i>UPDATE</a>
               <a href="/checkup/{{$athlete->id}}/add" class="waves-effect waves-light btn-small"><i class="tiny material-icons left">edit</i>Add Checkup</a></td>
           </tr>
